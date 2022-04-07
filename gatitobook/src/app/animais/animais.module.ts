b3abff20ea849a30728a8d1cc1d1b67dcde5699c
Joao Carlos Borges Chagas <joao.borges.chagas@avanade.com>
2022-04-06 23:43:32 -0300

    Autenticação guards para controle de rotas

--- gatitobook/src/app/animais/animais.module.ts
+++ gatitobook/src/app/animais/animais.module.ts
@@ -6,6 +6,7 @@ import { AnimaisRoutingModule } from './animais-routing.module';
 import { ListaAniamisComponent } from './lista-aniamis/lista-aniamis.component';
 import { AnimalComponent } from './animal/animal.component';
 import { GradeFotosAnimaisComponent } from './grade-fotos-animais/grade-fotos-animais.component';
+import { DetalheAnimalComponent } from './detalhe-animal/detalhe-animal.component';
 
 
 @NgModule({
@@ -13,6 +14,7 @@ import { GradeFotosAnimaisComponent } from './grade-fotos-animais/grade-fotos-an
     ListaAniamisComponent,
     AnimalComponent,
     GradeFotosAnimaisComponent,
+    DetalheAnimalComponent,
 
   ],
   imports: [
